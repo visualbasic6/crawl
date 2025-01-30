893cb89b3a29c815d9915978ce7627d9dec40443
pad <38997186+visualbasic6@users.noreply.github.com>
2025-01-30 13:55:13 -0500

    4

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
+    "context"
+    "fmt"
+    "os"
 
-	"github.com/migalabs/eth-light-crawler/cmd"
+    "github.com/sirupsen/logrus"
+    cli "github.com/urfave/cli/v2"
 
-	log "github.com/sirupsen/logrus"
-	cli "github.com/urfave/cli/v2"
+    "github.com/example/crawler/cmd"
 )
 
 var (
-	CliName    = "ethereum light crawler"
-	CliVersion = "v0.0.1"
+    CliName    = "ethereum sepolia crawler"
+    CliVersion = "v0.0.1"
 )
 
 func main() {
-	fmt.Println(CliName, CliVersion)
+    fmt.Println(CliName, CliVersion)
 
-	lightCrawler := cli.App{
-		Name:      CliName,
-		Usage:     "This tool uses Ethereum's peer-discovery protocol to measure the size of the Ethereum network (testnets included)",
-		UsageText: "eth-light-crawler [subcommands] [arguments]",
-		Commands: []*cli.Command{
-			cmd.Discovery5,
-		},
-	}
+    lightCrawler := cli.App{
+        Name:      CliName,
+        Usage:     "Discovers Ethereum (Sepolia) nodes via discv5 and saves them to a text file",
+        UsageText: "crawler [subcommands] [arguments]",
+        Commands: []*cli.Command{
+            cmd.Discovery5,
+        },
+    }
 
-	err := lightCrawler.RunContext(context.Background(), os.Args)
-	if err != nil {
-		log.Errorf("error running %s - %s", CliName, err.Error())
-		os.Exit(1)
-	}
+    err := lightCrawler.RunContext(context.Background(), os.Args)
+    if err != nil {
+        logrus.Errorf("error running %s - %s", CliName, err.Error())
+        os.Exit(1)
+    }
 }
