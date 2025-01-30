1d8d56d39977e3e3c6d7dddc30466b7749cb971b
pad <38997186+visualbasic6@users.noreply.github.com>
2025-01-30 13:56:42 -0500

    7

--- pkg/config/bootnodes.go
+++ pkg/config/bootnodes.go
@@ -1,9 +1,10 @@
 package config
 
 import (
-	"github.com/ethereum/go-ethereum/p2p/enode"
+    "github.com/ethereum/go-ethereum/p2p/enode"
 )
 
+// Official Sepolia bootnodes from Geth:
 var EthBootonodes = []*enode.Node{
-	enode.MustParse("enr:-KO4QJdykqPHhkrcGyqx6DtQbfbGcu8hBkIfWKEFCIVvD5-YCCwDjWMKHO5dziNFevWq7ZQnF6WKqasY74x50492CAqGAZRvPoR7g2V0aMfGhIjPgdmAgmlkgnY0gmlwhJO23gaJc2VjcDI1NmsxoQPGGB4FNP__4ya-s_VdDEK0PAfKqtGK4tW3R_t7vV1qioRzbmFwwIN0Y3CCdl-DdWRwgnZf"),
+    enode.MustParse("enode://c6181e0534ffffe326beb3f55d0c42b43c07caaad18ae2d5b747fb7bbd5d6a8a5092d3e7715d167e7b1c1b2a4ee18f7105e1c17f35fdb2ce5ef021e6718102c7@147.182.222.6:30303"),
 }
