3bd1ac89461fce9a8db94f17c8132bafb31995ed
pad <38997186+visualbasic6@users.noreply.github.com>
2025-01-30 13:57:19 -0500

    7

--- pkg/discv5/eth2_node.go
+++ pkg/discv5/eth2_node.go
@@ -1,62 +1,39 @@
 package discv5
 
 import (
-	"crypto/ecdsa"
-	"encoding/binary"
-	"math/bits"
-	"net"
-	"time"
+    "crypto/ecdsa"
+    "net"
+    "time"
 
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/migalabs/armiarma/src/utils"
-	"github.com/protolambda/zrnt/eth2/beacon/common"
+    "github.com/ethereum/go-ethereum/p2p/enode"
+    "github.com/migalabs/armiarma/src/utils"
+    "github.com/protolambda/zrnt/eth2/beacon/common"
 )
 
+// Minimal ENR wrapper
 type EnrNode struct {
-	Timestamp time.Time
-	ID        enode.ID
-	IP        net.IP
-	Seq       uint64
-	UDP       int
-	TCP       int
-	Pubkey    *ecdsa.PublicKey
-	Eth2Data  *common.Eth2Data
-	Attnets   *Attnets
+    Timestamp time.Time
+    ID        enode.ID
+    IP        net.IP
+    Seq       uint64
+    UDP       int
+    TCP       int
+    Pubkey    *ecdsa.PublicKey
+    Eth2Data  *common.Eth2Data
+    Attnets   *Attnets
 }
 
 func NewEnrNode(nodeID enode.ID) *EnrNode {
-
-	return &EnrNode{
-		Timestamp: time.Now(),
-		ID:        nodeID,
-		Pubkey:    new(ecdsa.PublicKey),
-		Eth2Data:  new(common.Eth2Data),
-		Attnets:   new(Attnets),
-	}
+    return &EnrNode{
+        Timestamp: time.Now(),
+        ID:        nodeID,
+        Pubkey:    new(ecdsa.PublicKey),
+        Eth2Data:  new(common.Eth2Data),
+        Attnets:   new(Attnets),
+    }
 }
 
 type Attnets struct {
-	Raw       utils.AttnetsENREntry
-	NetNumber int
-}
-
-func ParseAttnets(node enode.Node) (attnets *Attnets, exists bool, err error) {
-	att := new(Attnets)
-
-	attEntry := new(utils.AttnetsENREntry)
-
-	err = node.Load(attEntry)
-	if err != nil {
-		return att, false, nil
-	}
-	att.Raw = *attEntry
-
-	// count the number of bits in the Attnets
-	att.NetNumber = CountBits(att.Raw[:])
-	return att, true, nil
-}
-
-func CountBits(byteArr []byte) int {
-	rawInt := binary.BigEndian.Uint64(byteArr)
-	return bits.OnesCount64(rawInt)
+    Raw       utils.AttnetsENREntry
+    NetNumber int
 }
